608483078de1f5bd51253056a84fc1381a3fccd7
YOUR NAME <YOUR@EMAIL.com>
2022-11-03 09:59:28 -0700

    saving static only

--- index.js
+++ index.js
@@ -4,18 +4,6 @@ const port = 3000
 
 app.use(express.static(`public`))
 
-// app.get('/about', (req, res) =>{
-//     res.sendFile(path.join(__dirname, '/public/about/about.html'))
-// })
-
-// app.get('/dev', (req, res) =>{
-//     res.sendFile(path.join(__dirname, '/public/dev/dev.html'))
-// })
-
-// app.get('/', (req, res) =>{
-//     res.sendFile(path.join(__dirname, '/public/home/index.html'))
-// })
-
 app.use((req, res, next) =>{
     if (res.status(404)){
         res.redirect('/home/home.html')
