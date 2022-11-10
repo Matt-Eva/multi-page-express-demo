0d68c533071bca2f4d0266d278090cc7d7e23f43
YOUR NAME <YOUR@EMAIL.com>
2022-11-10 08:49:39 -0800

    saving updates

--- public/home/home.js
+++ public/home/home.js
@@ -1,1 +1,9 @@
 console.log('running home')
+
+fetch("http://localhost:3000/data")
+.then(r => r.json())
+.then(console.log)
+
+fetch("https://anapioficeandfire.com/api/characters/583")
+.then(r => r.json())
+.then(console.log)
